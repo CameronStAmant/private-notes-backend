20f8cba73d1de867e7ccca47217e6e65c7028086
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-14 10:59:04 -0600

    Add first tests

---
+++ tests/notes.test.js
@@ -0,0 +1,36 @@
+const initializeMongoServer = require('../mongoConfigTesting');
+initializeMongoServer();
+
+const noteRouter = require('../routes/note');
+
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use('/', noteRouter);
+
+beforeAll(async () => {
+  await request(app)
+    .post('/create')
+    .type('form')
+    .send({ title: 'title1', body: 'body1' })
+    .set('Accept', 'application/json');
+});
+
+test('notes route works', (done) => {
+  request(app)
+    .get('/')
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) return done(err);
+      expect(res.body[0]).toEqual(
+        expect.objectContaining({
+          title: 'title1',
+          body: 'body1',
+        })
+      );
+      done();
+    });
+});

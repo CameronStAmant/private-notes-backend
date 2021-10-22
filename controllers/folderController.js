d8cacdf50735ca479f02584785ceebd8c0ccffa6
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-22 08:47:10 -0600

    Add folder controller

---
+++ controllers/folderController.js
@@ -0,0 +1,31 @@
+const Folder = require('../models/folder');
+const { body, validationResult } = require('express-validator');
+
+exports.GET_folders = (req, res, next) => {
+  Folder.find().exec((err, list) => {
+    if (err) return next(err);
+    res.json(list);
+  });
+};
+
+exports.POST_folder = [
+  body('name').trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        name: req.body.name,
+      });
+    } else {
+      const folder = new Folder({
+        name: req.body.name,
+      });
+
+      folder.save((err) => {
+        if (err) return next(err);
+        res.sendStatus(200);
+      });
+    }
+  },
+];

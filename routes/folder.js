8b21bd963f904a9d192643f12ef959c3d184cd18
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-25 15:49:24 -0600

    Add GET_folder to route

--- routes/folder.js
+++ routes/folder.js
@@ -7,6 +7,8 @@ router.get('/', folder_controller.GET_folders);
 
 router.post('/', folder_controller.POST_folder);
 
+router.get('/:id', folder_controller.GET_folder);
+
 router.delete('/:id', folder_controller.DELETE_folder);
 
 router.put('/:id', folder_controller.PUT_folder);

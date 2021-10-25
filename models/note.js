8c9b7a3a45dd625f3b9b727b82b3f636ca30dce2
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-25 09:21:12 -0600

    Update note model to use folders

--- models/note.js
+++ models/note.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const NoteSchema = new Schema({
-  title: String,
-  body: String,
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  folder: { type: Schema.Types.ObjectId, ref: 'Folder', required: true },
 });
 
 NoteSchema.virtual('url').get(function () {

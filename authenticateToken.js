8e23185322909483f7f2f20f87be1e516e59aee4
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-12-13 12:00:28 -0700

    Clean up tests

--- authenticateToken.js
+++ authenticateToken.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['Authorization'];
+  const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-  if (token === null || token === undefined) return res.sendStatus(401);
+  if (token === null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
